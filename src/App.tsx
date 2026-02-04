172355d6c7a026950223a2a3066b6f968096fe6e
fromscratchsoftware-now <paulsempungu1@gmail.com>
2026-02-03 16:33:33 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 
 const App = () => (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-      </Routes>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
