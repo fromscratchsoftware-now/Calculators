bfe545ab4256173c140bd832386ed8e20f235a19
fromscratchsoftware-now <paulsempungu1@gmail.com>
2026-02-03 22:49:43 -0800

    Upgrade Tailwind to v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    plugins: [react()],
+    plugins: [tailwindcss(), react()],
     server: {
       port: 5173
     },
@@ -18,4 +19,3 @@ export default defineConfig(({ mode }) => {
     }
   };
 });
-
